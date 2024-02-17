70f8f358c488f6f1bfa7f4a4335ebcf3e609e370
andremfp <andremfp29@gmail.com>
2024-02-17 19:02:47 +0000

    server address as command line var

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
 
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
 	webserver := &Webserver{}
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", webserver)
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, webserver)
 	log.Fatal(err)
 }
