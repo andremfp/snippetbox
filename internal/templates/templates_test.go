7bdfe712f7509ab446f3ffe347145882e1e866c7
andremfp <andremfp29@gmail.com>
2024-04-08 18:19:57 +0100

    test template cache

---
+++ internal/templates/templates_test.go
@@ -0,0 +1,32 @@
+package templates_test
+
+import (
+	"testing"
+
+	"github.com/andremfp/snippetbox/internal/templates"
+)
+
+func TestNewTemplateCache(t *testing.T) {
+
+	numPages := 2
+
+	want := []string{
+		"home.html",
+		"view.html",
+	}
+
+	cache, err := templates.NewTemplateCache()
+	if err != nil {
+		t.Errorf("failed to create template cache: %v", err)
+	}
+
+	if len(cache) != numPages {
+		t.Errorf("want template cache with %d pages, got %d", numPages, len(cache))
+	}
+
+	for _, page := range want {
+		if _, ok := cache[page]; !ok {
+			t.Errorf("want template %s in template cache", page)
+		}
+	}
+}
